c3b07e87fa85b48673ac0c115b32cd6a53b3d7d3
rajeevsharma17 <48223749+rajeevsharma17@users.noreply.github.com>
2019-03-12 22:44:23 -0400

    Update freertos_blinky.c

--- freertos_blinky/example/src/freertos_blinky.c
+++ freertos_blinky/example/src/freertos_blinky.c
@@ -53,6 +53,11 @@ static void prvSetupHardware(void)
 
 	/* Initial LED0 state is off */
 	Board_LED_Set(0, false);
+	 /* Initial LED1 state is off */
+    Board_LED_Set(1, false);
+
+    /* Initial LED3 state is off */
+    Board_LED_Set(3, false);
 }
 
 /* LED1 toggle thread */
@@ -64,7 +69,7 @@ static void vLEDTask1(void *pvParameters) {
 		LedState = (bool) !LedState;
 
 		/* About a 3Hz on/off toggle rate */
-		vTaskDelay(configTICK_RATE_HZ / 6);
+		vTaskDelay(configTICK_RATE_HZ / 0);
 	}
 }
 
@@ -77,9 +82,22 @@ static void vLEDTask2(void *pvParameters) {
 		LedState = (bool) !LedState;
 
 		/* About a 7Hz on/off toggle rate */
-		vTaskDelay(configTICK_RATE_HZ / 14);
+		vTaskDelay(configTICK_RATE_HZ / 1.4);
 	}
 }
+/* LED3 toggle thread */
+static void vLEDTask3(void *pvParameters) {
+    bool LedState = false;
+
+    while (1) {
+        Board_LED_Set(3, LedState);
+        LedState = (bool) !LedState;
+
+        /* About a 14Hz on/off toggle rate */
+        vTaskDelay(configTICK_RATE_HZ / 2.8);
+    }
+}
+
 
 /* UART (or output) thread */
 static void vUARTTask(void *pvParameters) {
@@ -115,6 +133,10 @@ int main(void)
 	xTaskCreate(vLEDTask2, (signed char *) "vTaskLed2",
 				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
 				(xTaskHandle *) NULL);
+	/* LED3 toggle thread */
+    xTaskCreate(vLEDTask3, (signed char *) "vTaskLed3",
+                configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+                (xTaskHandle *) NULL);
 
 	/* UART output thread, simply counts seconds */
 	xTaskCreate(vUARTTask, (signed char *) "vTaskUart",
